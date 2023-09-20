2c69c56bf547e06dcd8b3e3630b7c1f5fbec432b
AJAY KUMAR SINGH <singhaj9452@gmail.com>
2023-09-20 11:09:33 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+import App from './App';
+
+// ReactDOM.render(what to show(like what I want to show), where to show(where I want to show))
+ReactDOM.render(<App />, document.getElementById('root'));
